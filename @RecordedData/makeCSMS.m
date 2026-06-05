f1ca0d2dc086c66087bfe66c8d7b1704d3544918
francescovaccari <55705432+francescovaccari@users.noreply.github.com>
2026-06-05 10:15:09 +0200

    Update makeCSMS.m
    Fixed handling of more sequences in 'first&last' option. Fixed handling of sequences beginning with different states

--- @RecordedData/makeCSMS.m
+++ @RecordedData/makeCSMS.m
@@ -57,6 +57,7 @@ end
 
 StartsOfTrials = [];
 EndsOfTrials = [];
+allPenultimateStates = [];
 
 for s = 1:totalSeq
 
@@ -73,7 +74,7 @@ for s = 1:totalSeq
             stateEnds = [stateStarts(2:end) - 1; numel(states)];
             stateStates = states(stateStarts);
             prevStates = [NaN; stateStates(1:end-1)];
-            
+
             startTrialIdx = [];
             endTrialIdx = [];
 
@@ -92,15 +93,17 @@ for s = 1:totalSeq
                     endTrialIdx = endTrialIdx(valid);
 
                 case "first&last"
+                    tmp_idx = find(stateStates == seq(1) | stateStates == seq(2))';
 
-                    allStartTrialIdx = find(stateStates == seq(1)); %find all trials that start with the initial state
-                    endTrialIdx = find(stateStates == seq(2)); %find all trials that end with the final state
+                    filteredStateStates = stateStates(tmp_idx);
+                    filteredStateStarts = stateStarts(tmp_idx);
+                    filteredStateEnds = stateEnds(tmp_idx);
+                    seq = seq(:);
 
-                    for i = 1:numel(endTrialIdx)
-                        candidateStartTrialsIdx = allStartTrialIdx(allStartTrialIdx < endTrialIdx(i));
-                        [~, myStartIdxIdx] = max(candidateStartTrialsIdx);
-                        startTrialIdx = [startTrialIdx candidateStartTrialsIdx(myStartIdxIdx)];
-                    end
+                    validTrialIdxIdx = find(arrayfun(@(i) isequal(filteredStateStates(i:i+numel(seq)-1), seq), 1:numel(filteredStateStates)-numel(seq)+1)).';
+
+                    [~, startTrialIdx] = intersect(stateStarts,filteredStateStarts(validTrialIdxIdx));
+                    [~, endTrialIdx] = intersect(stateEnds,filteredStateEnds(validTrialIdxIdx+1));
 
                 otherwise
                     error('matchOpt must be "strict" or "first&last".');
@@ -109,21 +112,22 @@ for s = 1:totalSeq
 
             StartsOfTrials = [StartsOfTrials; stateStarts(startTrialIdx)];
             EndsOfTrials = [EndsOfTrials; stateEnds(endTrialIdx)];
+            allPenultimateStates = [allPenultimateStates; prevStates(endTrialIdx)];
 
         case 'simple_events'
             error('eventType = simple_events not yet implemented')
-            
+
     end
 end
 switch eventType
     case 'per_frame_events'
         stateStartsTS = events.currentTime(stateStarts);
         stateEndsTS = events.currentTime(stateEnds);
-        
-        MS = cell(1, max(events.condID)); 
+
+        MS = cell(1, max(events.condID));
         CS = cell(size(spikes,2), max(events.condID));
-        TC = MS; %prepare for touchscreen touch coordinates 
-        TCTS = MS; %prepare for touchscreen touch coordinates timestamps 
+        TC = MS; %prepare for touchscreen touch coordinates
+        TCTS = MS; %prepare for touchscreen touch coordinates timestamps
         TRIAL_OUTCOME = MS;
 
         for t = 1:numel(StartsOfTrials)
@@ -134,7 +138,7 @@ switch eventType
                 errorType = '';
             end
             lastState = events.state(EndsOfTrials(t));
-            penultimateState = prevStates(endTrialIdx(t));
+            penultimateState = allPenultimateStates(t);
 
             if ~isempty(MS{1,cond})
                 trial = size(MS{1,cond},2)+1;
@@ -147,7 +151,7 @@ switch eventType
             else
                 MS{1,cond}{trial} = nan(1,numel(otherMarkers)+3);
             end
-            
+
             trialStates_idx = (stateStarts > StartsOfTrials(t) & stateStarts < EndsOfTrials(t) & stateEnds ~= EndsOfTrials(t));
             trialStates = stateStates(trialStates_idx)';
             tmp = find(ismember(otherMarkers, trialStates));
@@ -155,7 +159,7 @@ switch eventType
             MS{1,cond}{trial}(1) = stateStartsTS(stateStarts == StartsOfTrials(t));
             MS{1,cond}{trial}(end-1:end) = [stateStartsTS(stateEnds == EndsOfTrials(t)) stateEndsTS(stateEnds == EndsOfTrials(t))];
             MS{1,cond}{trial}(tmp+1) = stateStartsTS(trialStates_idx)';
-                                        
+
             % segment touchscreen trajectories
             if ismember('xCursor', events.Properties.VariableNames) && ismember('yCursor', events.Properties.VariableNames)
                 TC{1,cond}{trial}.xCursor = events.xCursor(events.currentTime > MS{1,cond}{trial}(1) & events.currentTime < MS{1,cond}{trial}(end))';
@@ -168,7 +172,7 @@ switch eventType
                 end
             end
             TCTS{1,cond}{trial} = events.currentTime(events.currentTime > MS{1,cond}{trial}(1) & events.currentTime < MS{1,cond}{trial}(end))';
-            
+
             % segment spikes
             for neu = 1:size(spikes,2)
                 CS{neu, cond}{trial} = spikes{1, neu}(spikes{1, neu} > MS{1,cond}{trial}(1) & spikes{1, neu} < MS{1,cond}{trial}(end));
